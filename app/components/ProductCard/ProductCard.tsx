4894f9de38fd6e7c3b340e7f3d9dd4552f450a2b
Kairan Moraes <kmoraestt@gmail.com>
2023-09-11 22:59:08 +0100

    Initial commit

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => console.log("test")}>
+        add
+      </button>
+    </div>
+  );
+};
+
+export default ProductCard;
