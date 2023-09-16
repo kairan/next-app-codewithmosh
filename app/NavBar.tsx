0dc9319c2e9ddd231f25a77a7ac53bb352a6699e
Kairan Moraes <kmoraestt@gmail.com>
2023-09-16 22:00:18 +0100

    add authentication on client component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,20 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <nav className="flex bg-slate-200 p-5 space-x-3">
-      <Link href="/" >
-        Next.js
-      </Link>
-      <Link href="/users">
-        Users
-      </Link>
-      <Link href="/api/auth/signin">Login</Link>
+      <Link href="/">Next.js</Link>
+      <Link href="/users">Users</Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && <div> {session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </nav>
   );
 };
