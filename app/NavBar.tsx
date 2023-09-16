e97c26ac8cb2cf2881a64e8d4a65cc2d0605dc93
Kairan Moraes <kmoraestt@gmail.com>
2023-09-16 22:06:09 +0100

    adding logout button

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -11,7 +11,12 @@ const NavBar = () => {
       <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
       {status === "loading" && <div>Loading...</div>}
-      {status === "authenticated" && <div> {session.user!.name}</div>}
+      {status === "authenticated" && (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout"> Logout</Link>
+        </div>
+      )}
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Login</Link>
       )}
