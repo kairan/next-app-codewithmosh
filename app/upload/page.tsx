54b1425f814c259fdd887ee3b2caa5521e2610b7
Kairan Moraes <kmoraestt@gmail.com>
2023-09-16 21:17:42 +0100

    Add cloudnary to project

---
+++ app/upload/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import React, { useState } from "react";
+
+type CloudinaryResult = {
+  public_id: string;
+};
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="some screenshots"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="awxaodf2"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== "success") {
+            return;
+          }
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
